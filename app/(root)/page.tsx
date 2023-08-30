664293a8473e78cdd1ff0649ad24211ef37a7e10
charlie <linkin8288@gmail.com>
2023-08-30 14:41:32 +0200

    Home Page and custom scroll bar

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <>
+      <h1 className='head-text text-left'>Home</h1>
+
+      <section className='mt-9 flex flex-col gap-10'>
+        <p className='no-result'>No threads found</p>
+      </section>
+    </>
+  )
+}
